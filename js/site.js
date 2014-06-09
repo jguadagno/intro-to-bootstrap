76d1324a3e8e89aa83e52ae49c61d20e161f9f32
jguadagno <JGuadagno@4T1SFV1.mi.corp.rockfin.com>
2014-06-09 06:03:24 -0700

    Added jquery.bootstrapThemeSwitcher and updated the menus

--- js/site.js
+++ js/site.js
@@ -1,4 +1,9 @@
-$().ready(function() {
+function onWindowResize() {
+    $('#width').text($(window).width());
+    $('#height').text($(window).height());
+}
+
+$().ready(function () {
 
     $("button[data-toggle=tooltip]").tooltip();
     $("a[data-toggle=popover]").popover();
@@ -7,30 +12,19 @@ $().ready(function() {
     $('#loading-example-btn').click(function () {
         var btn = $(this);
         btn.button('loading');
-        setTimeout(function() {btn.button('reset')}, 2000);
+        setTimeout(function () {btn.button('reset'); }, 2000);
     });
 
-    /* For theme switching */
-    var themeName = $.cookie("themeName");
-	var themePath = $.cookie("themePath");
-	if (themeName !== undefined) {
-		setTheme(themeName, themePath);
-	}
+    // Setup the theme selectors
+    $('#ThemeList').bootstrapThemeSwitcher();
+
+    // Change the theme
+    //$().bootstrapThemeSwitcher('switchTheme', 'default', '//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css');
+
+    // Load the previously selected theme from the cookies
+    $().bootstrapThemeSwitcher('loadThemeFromCookie');
 
     /* Updates the footer with the client size */
     $(window).resize(onWindowResize);
     onWindowResize();
 });
-
-function onWindowResize() {
-    $('#width').text($(window).width());
-    $('#height').text($(window).height());
-}
-
-function setTheme(themeName, themePath) {
-	var cssLink = "<link rel='stylesheet' id='bootstrapTheme' data-name='" + themeName + "' href='" + themePath + "bootstrap.min.css' type='text/css' />";
-	$('#bootstrapTheme').replaceWith(cssLink);
-
-	$.cookie("themeName", themeName, { expires: 7, path: "/" });
-	$.cookie("themePath", themePath, { expires: 7, path: "/" });
-}
