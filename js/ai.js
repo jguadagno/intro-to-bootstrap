24cc35e57d62d2b39b521b58fa4891c56ef82848
Joseph Guadagno <jguadagno@hotmail.com>
2019-06-15 11:06:49 -0700

    Correct the Application Insights Key

--- js/ai.js
+++ js/ai.js
@@ -1,7 +1,7 @@
 var appInsights=window.appInsights||function(config){
 function r(config){t[config]=function(){var i=arguments;t.queue.push(function(){t[config].apply(t,i)})}}var t={config:config},u=document,e=window,o="script",s=u.createElement(o),i,f;s.src=config.url||"https://az416426.vo.msecnd.net/scripts/a/ai.0.js";u.getElementsByTagName(o)[0].parentNode.appendChild(s);try{t.cookie=u.cookie}catch(h){}for(t.queue=[],i=["Event","Exception","Metric","PageView","Trace","Dependency"];i.length;)r("track"+i.pop());return r("setAuthenticatedUserContext"),r("clearAuthenticatedUserContext"),config.disableExceptionTracking||(i="onerror",r("_"+i),f=e[i],e[i]=function(config,r,u,e,o){var s=f&&f(config,r,u,e,o);return s!==!0&&t["_"+i](config,r,u,e,o),s}),t
 }({
-    instrumentationKey:"4f42033c-3579-4a94-8023-a3561518ae7f"
+    instrumentationKey:"3ecbf67f-18ea-4827-8011-e450a0c88c52"
 });
     
 window.appInsights=appInsights;
